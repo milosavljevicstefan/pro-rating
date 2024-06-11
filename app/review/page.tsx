138f3d184b6cb32ebac69f4b8fdca7ba984a23c6
Darko-darko <73240013+Darko-darko@users.noreply.github.com>
2024-06-11 09:39:36 +0200

    Update page.tsx

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -109,7 +109,7 @@ const ReviewContent = () => {
         thankYou: 'Danke für Ihren Besuch!',
         enterEmail: 'Geben Sie Ihre E-Mail ein und gewinnen Sie einen Preis.',
         pleaseRate: 'Bitte bewerten Sie uns und helfen Sie uns, gemeinsam zu wachsen.',
-        errorLabel: 'Bitte geben Sie eine gültige E-Mail-Adresse ein.',
+        //errorLabel: 'Bitte geben Sie eine gültige E-Mail-Adresse ein.',
         placeHolder: 'Geben Sie Ihre E-Mail-Adresse ein...',
         bad: 'Schlecht',
         okay: 'Okay',

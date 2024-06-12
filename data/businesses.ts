13927eaa756c34faa9878534e38348528efe03de
Darko-darko <73240013+Darko-darko@users.noreply.github.com>
2024-06-12 22:22:37 +0200

    Update businesses.ts

--- data/businesses.ts
+++ data/businesses.ts
@@ -64,7 +64,7 @@ export const businessDetails: { [key: string]: BusinessDetails } = {
   "RAKITA": {
     image: "/images/RAKITA.png",
     ownerEmails: ["darko.jocic.ns@gmail.com", "stefan.milosavljevic01@gmail.com"],
-    googleLink: "https://search.google.com/local/writereview?placeid=ChIJVRXIjNh3nkcRUqXPsNscmO0",
+    googleLink: "https://search.google.com/local/writereview?placeid=ChIJcWFv561tThMRlKW17QDBV3s",
     languages: ["cro"],
     reward: false,
     rewardText: 'Ako u narednih mesec dana dovedete novog polaznika na sledeci kurs imate 50% popusta!',

7b18de4b7cc1baf564a40c21837310837e1990c2
Darko-darko <73240013+Darko-darko@users.noreply.github.com>
2024-06-27 14:50:08 +0200

    Update businesses.ts

--- data/businesses.ts
+++ data/businesses.ts
@@ -119,3 +119,17 @@ export const businessDetails: { [key: string]: BusinessDetails } = {
     timeZone: "Europe/Berlin"
     },
 };
+
+"magaza": {
+    fullName: "magaza",
+    activated: true,
+    image: "/images/RAKITA.png",
+    ownerEmails: ["darko.jocic.ns@gmail.com", "kiubieryt@gmail.com", "novakovicdusan555@gmail.com" ],
+    googleLink: "https://search.google.com/local/writereview?placeid=ChIJcWFv561tThMRlKW17QDBV3s",
+    languages: ["sr"],
+    reward: false,
+    rewardText: 'Ako u narednih mesec dana dovedete novog polaznika na sledeci kurs imate 50% popusta!',
+    backgroundColor: "#e4f4fe", // Silver
+    textColor: "#000000",
+    timeZone: "Europe/Berlin"
+  },

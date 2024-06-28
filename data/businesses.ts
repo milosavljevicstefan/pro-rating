108c6d81b6fb0c27f67acb93b8d61ade606059d5
Darko-darko <73240013+Darko-darko@users.noreply.github.com>
2024-06-28 11:32:03 +0200

    Update businesses.ts

--- data/businesses.ts
+++ data/businesses.ts
@@ -121,7 +121,7 @@ export const businessDetails: { [key: string]: BusinessDetails } = {
 "magaza": {
     fullName: "magaza",
     activated: true,
-    image: "/images/Magaza FNR.png",
+    image: "/images/Magaza.png",
     ownerEmails: ["darko.jocic.ns@gmail.com", "kiubieryt@gmail.com", "novakovicdusan555@gmail.com" ],
     googleLink: "https://search.google.com/local/writereview?placeid=ChIJT4tm7S4QW0cRPtywUIbmQJA",
     languages: ["srb"],
